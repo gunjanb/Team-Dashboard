0a9fd9cb29e039773193748ad5b1199a8f7c92d1
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-05-26 09:07:08 -0700

    updated for questions related to engg and intern

--- index.js
+++ index.js
@@ -17,6 +17,179 @@ allTeamMembers = [];
 
 // // Create a function to write README file
 // function writeToFile(fileName, data) {}
+const Done = () => {
+  const generatedHtmlContent = generateMyTeamPage(allTeamMembers);
+};
+
+const getEngineerInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the team Engineer's Name :",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            return "Please enter the team Engg's name.";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "empId",
+        message: "Please enter the team Engineer's Employee Id :",
+        validate: (empId) => {
+          if (regexNumber.test(empId)) {
+            return true;
+          } else {
+            return "Please enter valid EmpId";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "emailId",
+        message: "Please enter the team Engineer's Email Id :",
+        validate: (emailId) => {
+          if (regexEmail.test(emailId)) {
+            return true;
+          } else {
+            return "Please enter a valid email Id.";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "githubUserName",
+        message: "Please enter  team Engineer's github User number :",
+        validate: (githubUserName) => {
+          if (githubUserName) {
+            return true;
+          } else {
+            return "Please enter username for Github.";
+          }
+        },
+      },
+    ])
+    .then((responsedata) => {
+      console.log(responsedata);
+      //   console.log(response.emailId);
+      //destructor the object and pass it to manager class
+      const { name, empId, emailId, githubUserName } = responsedata;
+      //  create an instance of manager with user input
+      const engineer = new Engineer(name, empId, emailId, githubUserName);
+      // create object of kind manager and add it to allTeamMember array
+      const addEngineer = {
+        role: engineer.getRole(),
+        name: engineer.getName(),
+        id: engineer.getId(),
+        email: engineer.getEmail(),
+        extrafield: engineer.getGithub(),
+      };
+      //store object in global array
+      allTeamMembers.push(addEngineer);
+      promptOtherQuestions();
+    });
+};
+
+const getInternInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the team Intern's Name :",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            return "Please enter the team Intern's name.";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "empId",
+        message: "Please enter the team Intern's Employee Id :",
+        validate: (empId) => {
+          if (regexNumber.test(empId)) {
+            return true;
+          } else {
+            return "Please enter valid EmpId";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "emailId",
+        message: "Please enter the team Intern's Email Id :",
+        validate: (emailId) => {
+          if (regexEmail.test(emailId)) {
+            return true;
+          } else {
+            return "Please enter a valid email Id.";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "schoolName",
+        message: "Please enter team Intern's School name:",
+        validate: (schoolName) => {
+          if (schoolName) {
+            return true;
+          } else {
+            return "Please enter school name.";
+          }
+        },
+      },
+    ])
+    .then((responsedata) => {
+      console.log(responsedata);
+      //   console.log(response.emailId);
+      //destructor the object and pass it to manager class
+      const { name, empId, emailId, schoolName } = responsedata;
+      //  create an instance of manager with user input
+      const intern = new Intern(name, empId, emailId, schoolName);
+      // create object of kind manager and add it to allTeamMember array
+      const addIntern = {
+        role: intern.getRole(),
+        name: intern.getName(),
+        id: intern.getId(),
+        email: intern.getEmail(),
+        extrafield: intern.getSchool(),
+      };
+      //store object in global array
+      allTeamMembers.push(addIntern);
+      promptOtherQuestions();
+    });
+};
+
+const promptOtherQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message:
+          "Please select a team member you would like to add based on role? Please select 'Done' if you are finished building your team . :",
+        choices: ["Engineer", "Intern", "Done"],
+      },
+    ])
+    .then((responsedata) => {
+      if (responsedata.employeeType === "Engineer") {
+        getEngineerInfo();
+      } else if (responsedata.employeeType === "Intern") {
+        getInternInfo();
+      } else if (responsedata.employeeType === "Done") {
+        Done();
+      } else {
+        //promptOtherQuestions();
+      }
+    });
+};
 
 // // Create a function to initialize app
 function startCollectingInfo() {
@@ -96,7 +269,6 @@ function startCollectingInfo() {
         email: manager.getEmail(),
         extrafield: manager.getofficeNumber(),
       };
-      console.log(addManager);
       //store object in global array
       allTeamMembers.push(addManager);
       promptOtherQuestions();
